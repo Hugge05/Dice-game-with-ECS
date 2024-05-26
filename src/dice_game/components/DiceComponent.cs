91b3fcf47d05d525dec62c0cfaa5a92ed7fd15cb
Hugge05 <75117937+Hugge05@users.noreply.github.com>
2024-05-26 13:35:23 +0200

    Update DiceComponent.cs
    kommenterade koden

--- src/dice_game/components/DiceComponent.cs
+++ src/dice_game/components/DiceComponent.cs
@@ -1,15 +1,15 @@
 namespace ECS.Components
 {
-    // The DiceComponent class represents a component that can be attached to an entity.
-    // This component simulates a dice with fixed values and stores the current dice value.
+    // DiceComponent-klassen representerar en komponent som kan fästas vid en entitet.
+    // Denna komponent simulerar en tärning med fasta värden och lagrar det aktuella tärningsvärdet.
     public class DiceComponent
     {
-        // An array representing the possible values of the dice.
-        // This is a read-only property initialized with values 1 through 6.
+        // En array som representerar de möjliga värdena på tärningen.
+        // Detta är en skrivskyddad egenskap som är initialiserad med värdena 1 till 6.
         public int[] dicevalues { get; } = { 1, 2, 3, 4, 5, 6 };
 
-        // A field to store the current value of the dice.
-        // This can be modified to represent the result of a dice roll.
+        // Ett fält för att lagra det aktuella värdet på tärningen.
+        // Detta kan ändras för att representera resultatet av ett tärningskast.
         public int current_dicevalue = 1;
     }
 }
