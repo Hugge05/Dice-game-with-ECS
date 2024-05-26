935edfe6eb93ad3efc9cba820c5dbaecddac0cdf
Hugge05 <75117937+Hugge05@users.noreply.github.com>
2024-05-26 13:32:45 +0200

    Update InputComponent.cs
    kommenterade koden

--- src/dice_game/components/InputComponent.cs
+++ src/dice_game/components/InputComponent.cs
@@ -2,12 +2,12 @@ using System;
 
 namespace ECS.Components
 {
-
+    // The InputComponent class represents a component that can be attached to an entity.
+    // This component is used to store user input, specifically a guessed value.
     public class InputComponent
     {
+        // A field to store the guessed value provided by the user.
+        // The default value is initialized to 0.
         public Int32 guess = 0;
     }
 }
-
-
-
