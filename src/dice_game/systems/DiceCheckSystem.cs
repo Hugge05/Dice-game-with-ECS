17e48ce92a8601ac1b66e114fe68d316ac67f887
Hugge05 <75117937+Hugge05@users.noreply.github.com>
2024-05-26 13:33:32 +0200

    Update DiceCheckSystem.cs
    kommenterade ut koden

--- src/dice_game/systems/DiceCheckSystem.cs
+++ src/dice_game/systems/DiceCheckSystem.cs
@@ -1,17 +1,25 @@
 using ECS.Entities;
 using System;
 using ECS.Components;
+
 namespace ECS.Systems
 {
+    // DiceCheckSystem-klassen representerar ett system i Entity-Component-System (ECS) arkitekturen.
+    // Detta system hanterar logiken för att slumpmässigt generera ett nytt tärningsvärde för en enhet.
     public class DiceCheckSystem
     {
+        // DiceNumber-metoden tar en enhet som parameter och uppdaterar dess tärningskomponents aktuella värde.
         public void DiceNumber(Entity entity)
         {
+            // Hämtar DiceComponent från den givna enheten.
             var diceComponent = entity.GetComponent<DiceComponent>();
+            
+            // Skapar en ny instans av Random-klassen för att generera slumpmässiga tal.
             Random rnd = new Random();
+            
+            // Genererar ett nytt slumpmässigt värde för tärningen och tilldelar det till current_dicevalue.
+            // rnd.Next(1, diceComponent.dicevalues.Length + 1) genererar ett tal mellan 1 och 6 (inklusive).
             diceComponent.current_dicevalue = rnd.Next(1, diceComponent.dicevalues.Length + 1);
         }
     }
 }
-
-
