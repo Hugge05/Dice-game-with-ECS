f2268c059700d49f822f05aa1b7d25e8aa8b3a8a
Hugge05 <75117937+Hugge05@users.noreply.github.com>
2024-05-26 13:34:32 +0200

    Update InputSystem.cs
    kommenterade koden

--- src/dice_game/systems/InputSystem.cs
+++ src/dice_game/systems/InputSystem.cs
@@ -1,15 +1,21 @@
 using ECS.Entities;
 using ECS.Components;
 using System;
+
 namespace ECS.Systems
 {
+    // InputSystem-klassen representerar ett system i Entity-Component-System (ECS) arkitekturen.
+    // Detta system hanterar logiken för att läsa användarens inmatning och uppdatera en enhets inputkomponent.
     public class InputSystem
     {
+        // InputNumber-metoden tar en enhet som parameter och uppdaterar dess InputComponent med användarens gissning.
         public void InputNumber(Entity entity)
         {
+            // Hämtar InputComponent från den givna enheten.
             InputComponent inputComponent = entity.GetComponent<InputComponent>();
+            
+            // Läser en rad text från konsolen, konverterar den till ett heltal och tilldelar det till guess-fältet i InputComponent.
             inputComponent.guess = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
-    
